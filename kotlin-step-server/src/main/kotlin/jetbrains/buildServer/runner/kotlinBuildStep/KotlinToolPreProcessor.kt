bee7d2aff1e3d0438430b25b51ade22c01cfb4c0
Anton Zamolotskikh <anton.zamolotskikh@jetbrains.com>
2021-03-11 09:16:47 +0000

    Check server responsibility before copying the tool

--- kotlin-step-server/src/main/kotlin/jetbrains/buildServer/runner/kotlinBuildStep/KotlinToolPreProcessor.kt
+++ kotlin-step-server/src/main/kotlin/jetbrains/buildServer/runner/kotlinBuildStep/KotlinToolPreProcessor.kt
@@ -1,15 +1,22 @@
 package jetbrains.buildServer.runner.kotlinBuildStep
 
+import jetbrains.buildServer.serverSide.ServerResponsibility
 import jetbrains.buildServer.tools.ServerToolPreProcessorAdapter
 import jetbrains.buildServer.tools.installed.ToolPaths
 import jetbrains.buildServer.util.FileUtil
 import jetbrains.buildServer.web.openapi.PluginDescriptor
+import org.jetbrains.annotations.NotNull
 import java.nio.file.Path
 
-class KotlinToolPreProcessor(val pluginDescriptor: PluginDescriptor, val toolPaths: ToolPaths): ServerToolPreProcessorAdapter() {
+class KotlinToolPreProcessor(val pluginDescriptor: PluginDescriptor, val toolPaths: ToolPaths,
+                             val serverResponsibility: ServerResponsibility): ServerToolPreProcessorAdapter() {
     override fun getName() = KotlinToolType.INSTANCE.type
 
     override fun doBeforeServerStartup() {
+
+        if (!serverResponsibility.canWriteToConfigDirectory())
+            return
+
         for (version in KOTLIN_VERSION_NUMBERS_BUNDLED) {
             val fileName = getToolFileName(version)
             val destination = toolPaths.getSharedToolPath(fileName)
