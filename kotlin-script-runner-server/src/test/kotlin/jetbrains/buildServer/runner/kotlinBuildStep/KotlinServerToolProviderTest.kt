7fccacb023b5670b380737a81aefdeec09622c56
Anton Zamolotskikh <anton.zamolotskikh@jetbrains.com>
2022-09-20 16:52:59 +0100

    TW-75743 Update Kotlin compiler tool to 1.7.10

--- kotlin-script-runner-server/src/test/kotlin/jetbrains/buildServer/runner/kotlinBuildStep/KotlinServerToolProviderTest.kt
+++ kotlin-script-runner-server/src/test/kotlin/jetbrains/buildServer/runner/kotlinBuildStep/KotlinServerToolProviderTest.kt
@@ -33,7 +33,7 @@ class KotlinServerToolProviderTest: BaseTestCase() {
 
     public fun `test tool versions`() {
         then(provider.defaultBundledVersionId).isEqualTo(ToolVersionIdHelper.getToolId(KotlinToolType.INSTANCE, "bundled"))
-        then(provider.bundledToolVersions.map { it.version }).isEqualTo(listOf("1.5.0"))
+        then(provider.bundledToolVersions.map { it.version }).isEqualTo(listOf("1.7.10"))
         then(provider.availableToolVersions).isNotEmpty()
     }
 
